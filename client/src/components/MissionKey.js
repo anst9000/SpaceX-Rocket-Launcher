03c1f6be1d1de9cff682848315409fda5f468051
anst9000 <coltlaboy@yahoo.se>
2020-09-29 10:09:07 +0200

    Prepare for Netlify deployment

--- client/src/components/MissionKey.js
+++ client/src/components/MissionKey.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 
 export default function MissionKey() {
   return (
-    <div className="my-3">
+    <div className="mission-key my-3">
       <p>
-        <span className="px-3 mr-2 bg-success" /> = Success
+        <span className="mission-key-blob px-3 mr-2 bg-success" /> = Success
       </p>
       <p>
-        <span className="px-3 mr-2 bg-danger" /> = Fail
+        <span className="mission-key-blob px-3 mr-2 bg-danger" /> = Fail
       </p>
     </div>
   );
---
+++ public/precache-manifest.4ad686627c1fbc49fba4fdec8ba249d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8a1f41927cc8870ead92",
+    "url": "/static/js/main.8a1f4192.chunk.js"
+  },
+  {
+    "revision": "8513a69eeb60207120c7",
+    "url": "/static/js/1.8513a69e.chunk.js"
+  },
+  {
+    "revision": "8a1f41927cc8870ead92",
+    "url": "/static/css/main.b57ccf8a.chunk.css"
+  },
+  {
+    "revision": "d20c35927c6dc1df9b73147dc7f5a12d",
+    "url": "/index.html"
+  }
+];
